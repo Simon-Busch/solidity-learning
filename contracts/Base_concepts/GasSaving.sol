fe214661344948754c56b345efa6fc01c22f7f13
simonbusch <Simon.busch@live.fr>
2022-04-14 17:10:06 +0200

    improve gas contract

--- contracts/Base_concepts/GasSaving.sol
+++ contracts/Base_concepts/GasSaving.sol
@@ -11,13 +11,13 @@ contract GasSaving {
   // load state var to memory instead of updating total for each loop - 50781 gas
   // short circuit - 50460 gas
   // loop increments - 50070 gas
-  // cache array length - 50065 gas
-  // load array elements to memory - 49898 gas
+  // cache array length - 48209 gas
+  // load array elements to memory - 48047 gas
 
   uint public total;
 
   // [1, 2, 3, 4, 5, 100]
-  function sumIfEvenAndLessThan99(uint[] memory nums ) external {
+  function sumIfEvenAndLessThan99(uint[] calldata nums ) external {
     uint _total = total;
     // // for (uint i = 0; i < nums.length ; i+= 1) {
     // for (uint i = 0; i < nums.length ; ++ i) { // ++ i instead of i += 1 saves gas 
