8c99896efa9dae2e2d5e2433024b94c719386965
simon busch <simon.busch@live.fr>
2022-08-21 07:40:18 +0200

    [ADD] hardhat example application

---
+++ contracts/Application/SimpleStorage-hardhat/hardhat.config.ts
@@ -0,0 +1,26 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL!;
+const PRIVATE_KEY = process.env.PRIVATE_KEY!;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY!;
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  //yarn hardhat run scripts/deploy.ts --network hardhat
+  networks: {
+    // yarn hardhat run scripts/deploy.ts --network goerli
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+    },
+  },
+  solidity: "0.8.9",
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+};
+
+export default config;
---
+++ contracts/Application/SimpleStorage-hardhat/scripts/deploy.ts
@@ -0,0 +1,52 @@
+import { ethers, run, network } from "hardhat";
+//hardhat comes built in with Hardhat network
+async function main() {
+  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+  console.log("deploying contract ...");
+  const simpleStorage = await SimpleStorageFactory.deploy();
+  await simpleStorage.deployed();
+  console.log(`Deployed contract to: ${simpleStorage.address}`);
+
+  // check if live network ( not hardhat network )
+  // console.log (network.config)
+  // check for goerli network
+  if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+    await simpleStorage.deployTransaction.wait(6)
+    // best practice to wait for a few block to be mined to make sure data are on etherscan
+    await verify(simpleStorage.address, [])
+  } else {
+    console.log('wrong network, use Goerli')
+  }
+
+  const currentValue = await simpleStorage.retrieve();
+  console.log(`current value is: ${currentValue}`);
+  const transactionResponse = await simpleStorage.store('7');
+  await transactionResponse.wait(1)
+  const updatedValue = await simpleStorage.retrieve();
+  console.log(`updated value is: ${updatedValue}`)
+}
+
+async function verify(contractAddress: string, args?: any) {
+  console.log("Verifying contract ...");
+
+  try {
+    // "run" allow to run hardhat tasks
+    await run("verify:verify", {
+      address: contractAddress,
+      contructorArguments: args,
+    });
+  } catch (err: any) {
+    if (err.message.toLowerCase().includes("already verified")) {
+      console.log("already verified");
+    } else {
+      console.log(err);
+    }
+  }
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
