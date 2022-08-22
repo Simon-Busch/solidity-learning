4ca758473365530c3e97f13c278e697b66ebea81
simon busch <simon.busch@live.fr>
2022-08-22 17:30:41 +0200

    [ADD] example of tests

--- contracts/Application/SimpleStorage-hardhat/hardhat.config.ts
+++ contracts/Application/SimpleStorage-hardhat/hardhat.config.ts
@@ -1,8 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 require("dotenv").config();
-// require('./tasks/accounts');
-// require('./tasks/block-number');
 import './tasks/accounts';
 import "./tasks/block-number";
 
@@ -21,11 +19,18 @@ const config: HardhatUserConfig = {
       accounts: [PRIVATE_KEY],
       chainId: 5,
     },
+    locahost: {
+      url: " http://127.0.0.1:8545/",
+      chainId: 31337
+      // no need to pass accounts here it's automatically done by hardhat
+    }
   },
   solidity: "0.8.9",
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
   },
 };
+// yarn hardhat node
+// run a local node
 
 export default config;
---
+++ contracts/Application/SimpleStorage-hardhat/test/SimpleStorage.ts
@@ -0,0 +1,36 @@
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { assert, expect } from "chai";
+import { ethers } from "hardhat";
+import { SimpleStorage } from "../typechain-types";
+
+describe("SimpleStorage contract", function () {
+  let SimpleStorageFactory;
+  let simpleStorage: SimpleStorage;
+  this.beforeEach(async function () {
+    SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+    simpleStorage = await SimpleStorageFactory.deploy();
+  });
+
+  it("Should start with a favorite number of 0", async function () {
+    const currentValue = await simpleStorage.retrieve();
+    const expectedValue = "0";
+    // expect()
+    // assert.equal(currentValue.toString(), expectedValue);
+    expect(currentValue.toString()).to.equal(expectedValue);
+  });
+
+  // NOTE ON TESTS
+  // it.only('Should update when we call store', async function () {
+  // if add .only will only run THIS test
+
+  // can also use this command : yarn hardhat test --grep store
+  // will run the tests with store in the description
+  it("Should update when we call store", async function () {
+    const expectedValue = "800";
+    const transactionResponse = await simpleStorage.store(expectedValue);
+    await transactionResponse.wait(1);
+    const currentValue = await simpleStorage.retrieve();
+    assert.equal(currentValue.toString(), expectedValue);
+  });
+});
