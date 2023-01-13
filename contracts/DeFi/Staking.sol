fd8be1049a4a54bff4aa0fa05a3501520351be74
simon busch <simon.busch@live.fr>
2023-01-13 09:26:15 +0100

    add example of staking

---
+++ contracts/DeFi/Staking.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "../Interface/IERC20.sol";
+
+
+// Staking = earn reward for staking a contract
+// Owner will set the rewards and the duration
+
+// Math
+// 
+
+contract Staking {
+
+}
